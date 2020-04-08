5c599599333bdb112b6b82eeef19d02b242ca559
real-chiu <real.chiu@oocl.com>
2020-04-08 16:12:35 +0800

    [Real] first failed test of check number that is not multiple of 3 or 5

---
+++ src/main/java/com/oocl/FizzBuzz.java
@@ -0,0 +1,7 @@
+package com.oocl;
+
+public class FizzBuzz {
+    public String checkFizzBuzz(int number) {
+        return "";
+    }
+}
---
+++ src/test/java/com/oocl/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package com.oocl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+    @Test
+    public void should_return_number_if_not_multiple_of_three_or_five () {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.checkFizzBuzz(1);
+        Assert.assertEquals(result, 1);
+    }
+
+}
