41a08254668917be0e0a12ea6dc06cca326bf52a
real-chiu <real.chiu@oocl.com>
2020-04-08 16:17:20 +0800

    [Real] failed test of should_return_Fizz_when_number_is_multiple_of_3

--- src/test/java/com/oocl/FizzBuzzTest.java
+++ src/test/java/com/oocl/FizzBuzzTest.java
@@ -8,7 +8,16 @@ public class FizzBuzzTest {
     public void should_return_number_if_not_multiple_of_three_or_five () {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.checkFizzBuzz(1);
-        Assert.assertEquals(result, "1");
+        Assert.assertEquals("1", result);
+    }
+
+    @Test
+    public void should_return_Fizz_if_multiple_of_three () {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.checkFizzBuzz(3);
+        String resultTwo = fizzBuzz.checkFizzBuzz(6);
+        Assert.assertEquals("Fizz", result);
+        Assert.assertEquals("Fizz", resultTwo);
     }
 
 }
