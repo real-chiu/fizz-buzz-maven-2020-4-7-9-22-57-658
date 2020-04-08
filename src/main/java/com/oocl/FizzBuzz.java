5be0e0c015d1b5665be8b5d5af5c81971d5c1b70
real-chiu <real.chiu@oocl.com>
2020-04-08 16:18:14 +0800

    [Real] pass test of should_return_Fizz_when_number_is_multiple_of_3

--- src/main/java/com/oocl/FizzBuzz.java
+++ src/main/java/com/oocl/FizzBuzz.java
@@ -2,6 +2,9 @@ package com.oocl;
 
 public class FizzBuzz {
     public String checkFizzBuzz(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
         return Integer.toString(number);
     }
 }
